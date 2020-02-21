3b831e6cc2cca23b38782fb210476caf903b7def
NiGo <1275810355@qq.com>
2020-02-21 21:02:26 +0800

    带注释版本

--- Progect/linkai/登录系统.c
+++ Progect/linkai/登录系统.c
@@ -10,28 +10,28 @@
 #include<ctype.h>
 #include<string.h>
 #define SIZE 50
-
-char get_choice();
+//为了安全的输入（防止暴力输入）
+char get_choice();	
 long get_tele();
 char get_first();
 char getch();
 char * s_gets(char * st, int n);
-
+//具体功能实现函数
 void create_user_file();
 void register_user();
 int login();
 void find_pwd();
-typedef struct user
+typedef struct user		//用户信息主体
 {
-	char id[SIZE];
-	char pwd[SIZE];
-	long tele;
+	char id[SIZE];	//账号
+	char pwd[SIZE];	//密码
+	long tele;	//电话号码
 }user;
 
 int main()
 {
-	char choice;
-	int is_register = 0;
+	char choice;	//记录get_choice()返回值
+	int is_register = 0;	//用于判断系统是否正确登录
 
 	create_user_file();
 	while ((choice = get_choice()) != 'q')
@@ -47,8 +47,8 @@ int main()
 		}
 		if (is_register)
 		{
-			is_register = 0;
-			system("clear");
+			is_register = 0;	//重新置0
+			system("clear");	//清屏
 			for (int i =0; i < 40;i++)
 				putchar('*');
 			putchar('\n');
@@ -56,22 +56,22 @@ int main()
 			for (int i =0; i < 40;i++)
 				putchar('*');
 			putchar('\n');
-			system("sleep 2s");
+			system("sleep 2s");	//休眠，用起来比较带感哈哈哈哈
 			break;
 		}
 	}
 	puts("请按任意键继续...");
-	getch();
+	getch();	//模拟任意键继续
 	puts("欢迎下次使用！");
 
 	return 0;
 }
-
+//保证文件users的存在，该文件存在时也不清空其中的数据
 void create_user_file()
 {
 	FILE * fp;
 
-	if ((fp = fopen("users", "r")) == NULL)
+	if ((fp = fopen("users", "r")) == NULL)	
 	{
 		if ((fp = fopen("users", "w+")) == NULL)
 		{
@@ -83,7 +83,7 @@ void create_user_file()
 	fclose(fp);
 	return;
 }
-
+//显示主界面并获取用户键入的信息
 char get_choice(void)
 {
 	char ch;
@@ -98,7 +98,8 @@ char get_choice(void)
 		putchar('*');
 	putchar('\n');
 	putchar('\n');
-    printf("%-20s%-20s\n%-20s%-20s\n\n", "a. 注册账号", "b. 登录账号", "c. 找回密码", "q. 退出登录");
+	//用字符串来输出是一个比较好用的技巧，来保证对齐
+    printf("%-20s%-20s\n%-20s%-20s\n\n", "a. 注册账号", "b. 登录账号", "c. 找回密码", "q. 退出登录");	
 	for (int i =0; i < 40;i++)
 		putchar('*');
 	putchar('\n');
@@ -111,7 +112,7 @@ char get_choice(void)
 
 	return ch;
 }
-
+//返回键入的第一个字母字符并清空缓存区
 char get_first(void)
 {
     char ch = 0;
@@ -121,14 +122,14 @@ char get_first(void)
 	while (getchar() != '\n')
 		continue;
 
-    return tolower(ch);
+    return tolower(ch); //返回小写值
 }
-
+//注册函数
 void register_user()
 {
 	FILE * fp;
-	user new_, old;
-	char temp[SIZE];
+	user new_, old;		//这里new后面多'_'的原因是c++有个关键字是new
+	char temp[SIZE]; 
 
 	system("clear");
 	for (int i =0; i < 40;i++)
@@ -175,17 +176,19 @@ void register_user()
 	fp = fopen("users", "a");
 	fwrite(&new_, sizeof(user), 1, fp);
 	printf("账号%s已注册成功！\n", new_.id);
-
+	system("sleep 3s");
+	
 	fclose(fp);
 	return;
 }
-
+//安全地获取电话号码
+//64位架构下long最大值是9223372036854775807，用来存储电话号码很合适
 long get_tele()
 {
 	long input;
 	char ch;
 
-	while (scanf("%ld", &input) != 1)
+	while (scanf("%ld", &input) != 1) 
 	{
 		while ((ch =getchar()) != '\n')
 			putchar(ch);
@@ -194,7 +197,7 @@ long get_tele()
 
 	return input;
 }
-
+//登录函数
 int login()
 {
 	FILE * fp;
@@ -213,11 +216,12 @@ int login()
 	if ((fp = fopen("users", "r")) == NULL)
 	{
 		fprintf(stderr, "读取用户信息失败！\n");
+		system("sleep 3s");
 		return 0;
 	}
 	puts("请输入您的账号：");
 	s_gets(temp, SIZE);
-	while (!feof(fp))
+	while (!feof(fp))	//判断是否到达文件结尾
 	{
 		fread(&old, sizeof(user), 1, fp);
 		if (!strcmp(temp, old.id))
@@ -227,6 +231,7 @@ int login()
 			if (!strcmp(temp, old.pwd))
 			{
 				printf("账户%s登录成功\n", old.pwd);
+				system("sleep 3s");
 				return 1;
 			}
 			else
@@ -243,7 +248,7 @@ int login()
 	fclose(fp);
 	return 0;
 }
-
+//找回密码函数
 void find_pwd()
 {
 	FILE * fp;
@@ -263,6 +268,7 @@ void find_pwd()
 	if ((fp = fopen("users", "r")) == NULL)
 	{
 		fprintf(stderr, "读取用户信息失败！\n");
+		system("sleep 3s");
 		return;
 	}
 
@@ -307,7 +313,7 @@ char * s_gets(char * st, int n)
 	}
 	return ret_val;
 }
-
+//自定义的getch()函数
 char getch()
 {
 	char c;
